b93386f7af37af11f62fc76f4a7c181cbfcc57f0
Daniel Taylor <kingorgg@gmail.com>
2016-06-20 14:50:16 +0100

    Update Language Files

--- app/Language/Da/messages.php
+++ app/Language/Da/messages.php
@@ -5,14 +5,16 @@ return array (
   'Database API' => '',
   'Events API' => '',
   'Hello, welcome from the welcome controller and subpage method! <br/>
-This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => '',
+This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => 'Hej og velkommen fra  welcome controlleren and subpage method! <br/>
+        Indeholdet kan ændres i <code>/app/Views/Welcome/SubPage.php</code>',
   'Hello, welcome from the welcome controller! <br/>
-This content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => '',
-  'Home' => '',
+This content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => 'Hej og velkommen fra welcome controller! <br/>
+        Indholdet kan ændres i <code>/app/Views/Welcome/Welcome.php</code>',
+  'Home' => 'Hjem',
   'If you have problems, please paste the above URL into your web browser.' => '',
   'Mailing API' => '',
   'Message sent while pretending. Please, look on <code>{0}</code>' => '',
-  'Open subpage' => '',
+  'Open subpage' => 'Åben undersiden',
   'Pagination' => 'Sideinddeling',
   'Password Reset' => '',
   'Password Sample' => '',
@@ -20,14 +22,14 @@ This content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => '
   'Please verify your E-mail address' => '',
   'Request API' => '',
   'Session API' => '',
-  'Subpage' => '',
+  'Subpage' => 'Under sid',
   'Test' => '',
   'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => '',
   'This is a test!!!' => '',
   'Timestamp:' => '',
   'To reset your password, complete this form: {0}.' => '',
   'Validation API' => '',
-  'Welcome' => '',
+  'Welcome' => 'Velkommen',
   'Welcome to {0}!' => '',
   'You are not authorized to access this resource.' => '',
 );
