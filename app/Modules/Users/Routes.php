0657c9b326444d2a45a3578b54773bb8aa9dcaeb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-24 13:01:39 +0200

    Move to the new parameters capture style: (:num) -> {0}

--- app/Modules/Users/Routes.php
+++ app/Modules/Users/Routes.php
@@ -75,23 +75,23 @@ Route::group(array('prefix' => 'admin', 'namespace' => 'App\Modules\Users\Contro
     Route::post('users/profile', array('before' => 'auth|csrf', 'uses' => 'Users@postProfile'));
 
     // The Users CRUD.
-    Route::get( 'users',                array('before' => 'auth',      'uses' => 'Users@index'));
-    Route::get( 'users/create',         array('before' => 'auth',      'uses' => 'Users@create'));
-    Route::post('users',                array('before' => 'auth|csrf', 'uses' => 'Users@store'));
-    Route::get( 'users/(:num)',         array('before' => 'auth',      'uses' => 'Users@show'));
-    Route::get( 'users/(:num)/edit',    array('before' => 'auth',      'uses' => 'Users@edit'));
-    Route::post('users/(:num)',         array('before' => 'auth|csrf', 'uses' => 'Users@update'));
-    Route::post('users/(:num)/destroy', array('before' => 'auth|csrf', 'uses' => 'Users@destroy'));
+    Route::get( 'users',              array('before' => 'auth',      'uses' => 'Users@index'));
+    Route::get( 'users/create',       array('before' => 'auth',      'uses' => 'Users@create'));
+    Route::post('users',              array('before' => 'auth|csrf', 'uses' => 'Users@store'));
+    Route::get( 'users/{id}',         array('before' => 'auth',      'uses' => 'Users@show'));
+    Route::get( 'users/{id}/edit',    array('before' => 'auth',      'uses' => 'Users@edit'));
+    Route::post('users/{id}',         array('before' => 'auth|csrf', 'uses' => 'Users@update'));
+    Route::post('users/{id}/destroy', array('before' => 'auth|csrf', 'uses' => 'Users@destroy'));
 
     // The Users Search.
     Route::post( 'users/search', array('before' => 'auth', 'uses' => 'Users@search'));
 
     // The Roles CRUD.
-    Route::get( 'roles',                array('before' => 'auth',      'uses' => 'Roles@index'));
-    Route::get( 'roles/create',         array('before' => 'auth',      'uses' => 'Roles@create'));
-    Route::post('roles',                array('before' => 'auth|csrf', 'uses' => 'Roles@store'));
-    Route::get( 'roles/(:num)',         array('before' => 'auth',      'uses' => 'Roles@show'));
-    Route::get( 'roles/(:num)/edit',    array('before' => 'auth',      'uses' => 'Roles@edit'));
-    Route::post('roles/(:num)',         array('before' => 'auth|csrf', 'uses' => 'Roles@update'));
-    Route::post('roles/(:num)/destroy', array('before' => 'auth|csrf', 'uses' => 'Roles@destroy'));
+    Route::get( 'roles',              array('before' => 'auth',      'uses' => 'Roles@index'));
+    Route::get( 'roles/create',       array('before' => 'auth',      'uses' => 'Roles@create'));
+    Route::post('roles',              array('before' => 'auth|csrf', 'uses' => 'Roles@store'));
+    Route::get( 'roles/{id}',         array('before' => 'auth',      'uses' => 'Roles@show'));
+    Route::get( 'roles/{id}/edit',    array('before' => 'auth',      'uses' => 'Roles@edit'));
+    Route::post('roles/{id}',         array('before' => 'auth|csrf', 'uses' => 'Roles@update'));
+    Route::post('roles/{id}/destroy', array('before' => 'auth|csrf', 'uses' => 'Roles@destroy'));
 });
