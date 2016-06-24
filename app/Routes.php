942588d0af0926836f894fcb60b4914462984d36
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-24 14:14:40 +0200

    Adjust the Routes to the new style of Route Patterns

--- app/Routes.php
+++ app/Routes.php
@@ -14,24 +14,25 @@ Route::any('', 'App\Controllers\Welcome@index');
 Route::any('subpage', 'App\Controllers\Welcome@subPage');
 
 // Demo Routes
-Route::any('demo/database',        'App\Controllers\Demo@database');
-Route::any('demo/password/(:any)', 'App\Controllers\Demo@password');
-Route::any('demo/events',          'App\Controllers\Demo@events');
-Route::any('demo/mailer',          'App\Controllers\Demo@mailer');
-Route::any('demo/session',         'App\Controllers\Demo@session');
-Route::any('demo/validate',        'App\Controllers\Demo@validate');
-Route::any('demo/paginate',        'App\Controllers\Demo@paginate');
-Route::any('demo/cache',           'App\Controllers\Demo@cache');
+Route::any('demo/database',            'App\Controllers\Demo@database');
+Route::any('demo/password/{password}', 'App\Controllers\Demo@password');
+Route::any('demo/events',              'App\Controllers\Demo@events');
+Route::any('demo/mailer',              'App\Controllers\Demo@mailer');
+Route::any('demo/session',             'App\Controllers\Demo@session');
+Route::any('demo/validate',            'App\Controllers\Demo@validate');
+Route::any('demo/paginate',            'App\Controllers\Demo@paginate');
+Route::any('demo/cache',               'App\Controllers\Demo@cache');
 
-Route::any('demo/request(/(:any)(/(:any)(/(:all))))', 'App\Controllers\Demo@request');
+Route::any('demo/request/{param1?}/{param2?}/{slug?}','App\Controllers\Demo@request')
+    ->where('slug', '.*');
 
-Route::any('demo/test/(:any)(/(:any)(/(:any)(/(:all))))', array(
+Route::any('demo/test/{param1}/{param2?}/{param3?}/{slug?}', array(
     'filters' => 'test',
     'uses'    => 'App\Controllers\Demo@test'
-));
+))->where('slug', '.*');
 
 // The Framework's Language Changer.
-Route::any('language/(:any)', array(
+Route::any('language/{language}', array(
     'filters' => 'referer',
     'uses'    => 'App\Controllers\Language@change'
 ));
