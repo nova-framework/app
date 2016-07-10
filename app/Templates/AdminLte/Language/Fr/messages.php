b054e9e082d0c4f2953a8e714b49bafad59c5e08
DonWagner <michael@soerensens.dk>
2016-07-10 22:33:18 +0200

    Update Fr/messages.php

--- app/Templates/AdminLte/Language/Fr/messages.php
+++ app/Templates/AdminLte/Language/Fr/messages.php
@@ -1,9 +1,17 @@
 <?php
 
 return array (
+  'ADMINISTRATION' => '',
+  'Control Panel' => '',
+  'Dashboard' => '',
   'Forgot Password?' => '',
   'Logout' => '',
+  'Member since {0}' => '',
   'Profile' => '',
+  'Roles' => '',
+  'Settings' => '',
   'Sign In' => '',
   'Sign Up' => '',
+  'Sign out' => '',
+  'Users' => '',
 );
