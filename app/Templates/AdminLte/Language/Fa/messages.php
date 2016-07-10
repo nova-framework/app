57b20c8e2c0b6ee684e9feda6105bdaf2587c037
DonWagner <michael@soerensens.dk>
2016-07-10 22:30:55 +0200

    Update Fa/messages.php

--- app/Templates/AdminLte/Language/Fa/messages.php
+++ app/Templates/AdminLte/Language/Fa/messages.php
@@ -1,9 +1,17 @@
 <?php
 
 return array (
+  'ADMINISTRATION' => '',
+  'Control Panel' => '',
+  'Dashboard' => '',
   'Forgot Password?' => 'فراموشی رمزعبور؟',
   'Logout' => 'خروج',
+  'Member since {0}' => '',
   'Profile' => 'پروفایل',
+  'Roles' => '',
+  'Settings' => '',
   'Sign In' => '',
   'Sign Up' => '',
+  'Sign out' => '',
+  'Users' => '',
 );
