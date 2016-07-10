1566ca407e4c465146c4c906962172a6e37bc1c1
DonWagner <michael@soerensens.dk>
2016-07-10 22:26:23 +0200

    Update Da/messages.php

--- app/Templates/AdminLte/Language/Da/messages.php
+++ app/Templates/AdminLte/Language/Da/messages.php
@@ -1,9 +1,17 @@
 <?php
 
 return array (
+  'ADMINISTRATION' => '',
+  'Control Panel' => 'Kontrolpanel',
+  'Dashboard' => 'Skrivebord',
   'Forgot Password?' => 'Glemt kodeord',
   'Logout' => 'Log ud',
+  'Member since {0}' => 'Medlem siden {0}',
   'Profile' => 'Bruger konto',
+  'Roles' => 'Bruger ',
+  'Settings' => 'Indstillinger',
   'Sign In' => 'Log ind',
   'Sign Up' => 'Tilmeld',
+  'Sign out' => 'Log ud',
+  'Users' => 'Brugere',
 );
