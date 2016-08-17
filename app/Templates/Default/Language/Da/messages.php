5fbc72e5fa38c7c624c6161d7bbe30183941c2e2
DonWagner <michael@soerensens.dk>
2016-08-17 10:58:42 +0200

    Update messages.php

--- app/Templates/Default/Language/Da/messages.php
+++ app/Templates/Default/Language/Da/messages.php
@@ -2,7 +2,7 @@
 
 return array (
   'Dashboard' => 'Skrivebord',
-  'Forgot Password?' => 'Glemt kodeord',
+  'Forgot Password?' => 'Glemt kodeord?',
   'Logout' => 'Log ud',
   'Profile' => 'Bruger konto',
   'User Login' => 'Log ind',
