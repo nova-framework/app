c7413b08bdfdd965c513a5e20624fcdb55df536f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-01-21 00:07:25 +0200

    Simplify app/Events.php

--- app/Events.php
+++ app/Events.php
@@ -21,22 +21,4 @@ Event::listen('router.matched', function($route, $request)
     }
 
     View::share('version', trim($version));
-
-    // Share the Views the current URI.
-    View::share('currentUri', $request->path());
-
-    // Share the Views the Backend's base URI.
-    $segments = $request->segments();
-
-    if(! empty($segments)) {
-        // Make the path equal with the first part if it exists, i.e. 'admin'
-        $baseUri = array_shift($segments) .'/';
-
-        // Add to path the next part, if it exists, defaulting to 'dashboard'.
-        $baseUri .= ! empty($segments) ? array_shift($segments) : 'dashboard';
-    } else {
-        $baseUri = '';
-    }
-
-    View::share('baseUri', $baseUri);
 });
