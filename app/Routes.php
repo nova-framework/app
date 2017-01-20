82b44e2f8841bacbb3bce59644c0abeaaf8dca86
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-01-21 00:04:45 +0200

    Improve app/Routes.php

--- app/Routes.php
+++ app/Routes.php
@@ -33,16 +33,17 @@ Route::any('language/{language}', array('before' => 'referer', function($languag
 {
     $languages = Config::get('languages');
 
-    // Only set language if it's in the Languages array
-    if (preg_match ('/[a-z]/', $language) && in_array($language, array_keys($languages))) {
+    // Only set language if it's in the Languages array.
+    if (in_array($language, array_keys($languages))) {
         Session::set('language', $language);
 
-        // Store the current Language in a Cookie lasting five years.
+        // Store also the current Language in a Cookie lasting five years.
         Cookie::queue(PREFIX .'language', $language, Cookie::FIVEYEARS);
     }
 
     return Redirect::back();
-}));
+
+}))->where('language', '([a-z]+)');
 
 /** End default Routes */
 
