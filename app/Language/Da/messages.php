e1ef9829ea345431d1a98af18a2308364c23f1c7
DonWagner <michael@soerensens.dk>
2017-01-25 06:20:33 +0100

    Update messages.php

--- app/Language/Da/messages.php
+++ app/Language/Da/messages.php
@@ -1,11 +1,11 @@
 <?php
 
 return array (
-  'If you have problems, please paste the above URL into your web browser.' => '',
-  'Password Reset' => '',
-  'Please verify your E-mail address' => '',
-  'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => '',
-  'To reset your password, complete this form: {0}.' => '',
+  'If you have problems, please paste the above URL into your web browser.' => 'Hvis du har problemer skal du indsætte den ovenstående URL ind i din webbrowser.',
+  'Password Reset' => 'Nulstil kodeord',
+  'Please verify your E-mail address' => 'Bekræft venligst din mailadresse',
+  'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => 'Tak fordi du har oprettet en bruger på {0}. Brug venligst linket her under til at bekræfte din mailadresse: <a href="{1}">{1}</a>',
+  'To reset your password, complete this form: {0}.' => 'For at nulstille dit kodeord, skal du udfyld denne formular: {0}.',
   'Welcome' => 'Velkommen',
-  'Yep! It works.' => '',
+  'Yep! It works.' => 'Jeps! Det virker.',
 );
