df569314cadd2ba23941709013175cf55d62afe4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-01-29 05:34:49 +0200

    Update the server.php

--- server.php
+++ server.php
@@ -7,9 +7,12 @@
 // php -S localhost:8080 -t public/ server.php
 
 
-$public_path = 'public/';
+$public_path = 'webroot/';
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
 $uri = urldecode($uri);
+
 $requested = $public_path.$uri;
 
 if (($uri !== '/') && file_exists($requested)) {
