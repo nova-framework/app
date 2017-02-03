a956dfd6f62c5400a77967ca094e91be029b5ea5
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-03 14:11:17 +0200

    Remove the App\Models\Role and the FileField extension, as being examples

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,12 +14,10 @@ use Nova\Auth\Reminders\RemindableTrait;
 use Nova\Auth\Reminders\RemindableInterface;
 use Nova\Database\ORM\Model as BaseModel;
 
-use Shared\Database\ORM\FileField\FileFieldTrait;
-
 
 class User extends BaseModel implements UserInterface, RemindableInterface
 {
-    use UserTrait, RemindableTrait, FileFieldTrait;
+    use UserTrait, RemindableTrait;
 
     //
     protected $table = 'users';
@@ -29,54 +27,5 @@ class User extends BaseModel implements UserInterface, RemindableInterface
     protected $fillable = array('role_id', 'username', 'password', 'realname', 'email', 'active', 'image', 'activation_code');
 
     protected $hidden = array('password', 'activation_code', 'remember_token');
-
-    public $files = array(
-        'image' => array(
-            'path'        => ROOTDIR .'assets/images/users/:unique_id-:file_name',
-            'defaultPath' => ROOTDIR .'assets/images/users/no-image.png',
-        ),
-    );
-
-    // Cache for associated Role instance.
-    private $cachedRole;
-
-
-    public function role()
-    {
-        return $this->hasOne('App\Models\Role', 'id', 'role_id');
-    }
-
-    public function hasRole($roles)
-    {
-        if (! isset($this->cachedRole)) {
-            $this->cachedRole = $this->role()->getResults();
-        }
-
-        // Check if the User is a Root account.
-        if (! is_null($this->cachedRole) && ($this->cachedRole->slug == 'root')) {
-            return true;
-        }
-
-        if (! is_array($roles)) {
-            return $this->checkUserRole($roles);
-        }
-
-        foreach ($roles as $role) {
-            if ($this->checkUserRole($role)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private function checkUserRole($wantedRole)
-    {
-        if(isset($this->cachedRole) && ($this->cachedRole instanceof Role)) {
-            return (strtolower($wantedRole) == strtolower($this->cachedRole->slug));
-        }
-
-        return false;
-    }
 
 }
