23865f6e1529ebd4b605090a3ad656219722eec6
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-03 14:29:06 +0200

    Remove the 'image' filed from the 'users' table on scripts/nova_users.sql

--- scripts/nova_users.sql
+++ scripts/nova_users.sql
@@ -33,7 +33,6 @@ CREATE TABLE `nova_users` (
   `realname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `active` tinyint(4) UNSIGNED NOT NULL DEFAULT '0',
-  `image` varchar(255) DEFAULT NULL,
   `activation_code` varchar(255) DEFAULT NULL,
   `remember_token` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -45,11 +44,7 @@ CREATE TABLE `nova_users` (
 --
 
 INSERT INTO `nova_users` (`id`, `username`, `password`, `realname`, `email`, `active`, `image`, `activation_code`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$MZpxcVZpwTCCotIkkfPP5O1sDC7GiKzD9klh4MoM/aE44YaVm4Xga', 'Administrator', 'admin@novaframework.dev', 1, NULL, NULL, NULL, '2016-06-03 10:15:00', '2016-09-05 22:38:39'),
-(2, 'marcus', '$2y$10$B1Q7LNu2xuIcFJ1lAotb5O93kkvUfFdOzUZhTmSdkQZ.6woLmgu3S', 'Marcus Spears', 'marcus@novaframework.dev', 1, NULL, NULL, NULL, '2016-06-03 10:19:00', '2016-06-03 10:19:00'),
-(3, 'michael', '$2y$10$klop7YxFoZOVqDq3hA7efeKEz4csFhAelfwP8M4s1ROlgpkBx9qVW', 'Michael White', 'michael@novaframework.dev', 1, NULL, NULL, NULL, '2016-06-03 10:20:00', '2016-06-05 14:22:19'),
-(4, 'john', '$2y$10$WzBPFMiFeJ2XK9eW34zEgelSJI3R1TVrOWbjVDxFXDeMQxoh8asYK', 'John Kennedy', 'john@novaframework.dev', 1, NULL, NULL, NULL, '2016-06-03 10:21:00', '2016-06-05 14:22:32'),
-(5, 'mark', '$2y$10$z4bRYEcnoHOR.GuObWTATuH/x1lto.2wUJ1RxCYWOmfjay2LnTd8W', 'Mark Black', 'mark@novaframework.dev', 1, NULL, NULL, NULL, '2016-06-03 10:22:00', '2016-06-05 13:16:05');
+(1, 'admin', '$2y$10$MZpxcVZpwTCCotIkkfPP5O1sDC7GiKzD9klh4MoM/aE44YaVm4Xga', 'Administrator', 'admin@novaframework.dev', 1, NULL, NULL, '2016-06-03 10:15:00', '2016-09-05 22:38:39');
 
 --
 -- Indexes for dumped tables
