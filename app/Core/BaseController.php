879523e56f95d47f0c3cb8a3c1e5e08c8e2c4ad0
DonWagner <michael@soerensens.dk>
2017-03-27 22:45:26 +0200

    Typo in BaseController

--- app/Core/BaseController.php
+++ app/Core/BaseController.php
@@ -45,7 +45,7 @@ abstract class BaseController extends Controller
         $path = str_replace('\\', '/', static::class);
 
         // Check for a valid controller on App or Modules.
-        if (preg_match('#^(.+)/Http/sControllers/(.*)$#i', $path, $matches)) {
+        if (preg_match('#^(.+)/Http/Controllers/(.*)$#i', $path, $matches)) {
             $view = $matches[2] .'/' .ucfirst($method);
 
             if ($matches[1] == 'App') {
