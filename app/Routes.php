f9d4bff69a081861aa8026fd499ca9566ec6c348
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-04-25 14:57:12 +0300

    Overall improvements

--- app/Routes.php
+++ app/Routes.php
@@ -17,7 +17,7 @@ Route::any('', function() {
         ->shares('title', __('Welcome'))
         ->withContent($content);
 
-    return View::makeLayout('Default')->withContent($view);
+    return View::makeLayout('Welcome')->withContent($view);
 });
 
 // The Language Changer
