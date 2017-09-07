2b13c68088d02a4b9100b8117122c93dee6a46a0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-07 07:31:29 +0300

    Remove the options infrastructure

--- app/Boot/Global.php
+++ app/Boot/Global.php
@@ -1,43 +1,5 @@
 <?php
 
 //--------------------------------------------------------------------------
-// Load The Options
-//--------------------------------------------------------------------------
-
-use Nova\Database\QueryException;
-
-use App\Models\Option;
-
-
-if (CONFIG_STORE === 'database') {
-    // Retrieve the Option items, caching them for 24 hours.
-
-    try {
-        $options = Cache::remember('system_options', 1440, function ()
-        {
-            return Option::all();
-        });
-    }
-    catch (QueryException $e) {
-        $options = array();
-    }
-
-    foreach ($options as $option) {
-        $key = $option->group;
-
-        if (! empty($option->item)) {
-            $key .= '.' .$option->item;
-        }
-
-        Config::set($key, $option->value);
-    }
-}
-
-// If the CONFIG_STORE is not in files mode, go Exception.
-else if(CONFIG_STORE !== 'files') {
-    throw new InvalidArgumentException('Invalid Config Store type.');
-}
-
-//--------------------------------------------------------------------------
 // Boot Stage Customization
 //--------------------------------------------------------------------------
