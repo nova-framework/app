381fd818f03870706f6ff74739583d511fd01cab
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-07 06:20:44 +0200

    Overall improvements

--- app/Config.php
+++ app/Config.php
@@ -13,10 +13,3 @@
  * PREFER to be used in Database calls or storing Session data, default is 'nova_'
  */
 define('PREFIX', 'nova_');
-
-/**
- * Setup the Config API Mode.
- *
- * For using the 'database' mode, you need a database having the table 'nova_options'
- */
-define('CONFIG_STORE', 'files'); // Supported: "files", "database"
