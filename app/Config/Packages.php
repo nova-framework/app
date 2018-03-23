4549a2226112da58ffc33412335794a1d91d2b67
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-23 10:54:32 +0200

    Update the app/Config/Packages.php

--- app/Config/Packages.php
+++ app/Config/Packages.php
@@ -73,38 +73,12 @@ return array(
     */
 
     'options' => array(
-        'platform' => array(
-            'enabled'  => true,
-            'order'    => 7001,
-        ),
-        'fields' => array(
-            'enabled'  => true,
-            'order'    => 7002,
-        ),
-        'permissions' => array(
-            'enabled'  => true,
-            'order'    => 8001,
-        ),
-        'roles' => array(
-            'enabled'  => true,
-            'order'    => 8002,
-        ),
-        'users' => array(
-            'enabled'  => true,
-            'order'    => 8003,
-        ),
-        'content' => array(
-            'enabled'  => true,
-            'order'    => 8004,
-        ),
-        'chat' => array(
+        /*
+        'blog' => array(
             'enabled'  => true,
             'order'    => 9001,
         ),
-        'messages' => array(
-            'enabled'  => true,
-            'order'    => 9001,
-        ),
+        */
     ),
 
 );
