0d42914d686de21fc39f42b48d61d8b5dd3b56a9
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-07-14 20:22:37 +0300

    Improve the configuration

--- app/Config/Routing.php
+++ app/Config/Routing.php
@@ -28,16 +28,7 @@ return array(
         //          ^____________________^____^____________________Those are the parts of path which are validated.
         //
         'paths' => array(
-
-            // AdminLTE
-            'almasaeed2010/adminlte' => array(
-                'bower_components',
-                'dist',
-                'plugins'
-            ),
-
-            // Bootstrap
-            'twbs/bootstrap' => 'dist',
+            //
         ),
     ),
 );
